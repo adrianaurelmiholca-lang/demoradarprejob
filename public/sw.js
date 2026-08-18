9116c7670eeca494aeab822a058473a65b02f33a
Adrian Miholca <adrianaurelmiholca@gmail.com>
2026-08-18 12:26:11 +0300

    Folosire logo PREJOB la instalarea pe telefon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE="prejob-radar-final-2026-08-15";
-const ASSETS=["./","index.html","manifest.webmanifest","icon.svg","logo-prejob-mark.png"];
+const CACHE="prejob-radar-2026-08-18-logo";
+const ASSETS=["./","index.html","manifest.webmanifest","logo-prejob-mark.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match("index.html"))))});
